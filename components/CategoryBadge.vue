1eed861d996f3566a28e2f9e20f251016f6b16a6
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-12-22 21:43:20 +0100

    chore: create the code of the `category badge` component

---
+++ components/CategoryBadge.vue
@@ -0,0 +1,34 @@
+<template>
+  <div 
+    class="flex justify-center items-center h-max rounded-lg p-3 w-max"
+    :style="{ backgroundColor: badgeBackgroundColor }">
+    {{ category.name }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CategoryBadge',
+  props: {
+    category: {
+      type: Object,
+      default: () => ({
+        name: String,
+        color: String
+      })
+    }
+  },
+  computed: {
+    badgeBackgroundColor() {
+      if (this.category.color) {
+        return `#${String(this.category.color)}`
+      }
+      return '#D9DDDC'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
