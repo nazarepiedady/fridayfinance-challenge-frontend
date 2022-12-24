0b5f705ebc9127f976337c4269b374fc887519ef
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-12-24 11:23:18 +0100

    chore: configure application of the styles of tailwind css

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,9 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: [
+    '@/assets/styles/main.css'
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
@@ -49,7 +51,6 @@ export default {
       plugins: {
         tailwindcss: {},
         autoprefixer: {},
-
       }
     }
   },
