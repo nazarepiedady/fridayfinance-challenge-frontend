74d3f3a754238c95091bb0bf6c78793502b65955
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-12-24 08:31:14 +0100

    chore: write the first script part of `pages/index.vue`

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,25 @@
 </template>
 
 <script>
+import getBanks from '@/apollo/queries/getBanks'
+import getAccounts from '@/apollo/queries/getAccounts'
+import getTransactions from '@/apollo/queries/getTransactions'
+import TransactionTable from '@/components/TransactionTable.vue'
+
 export default {
   name: 'IndexPage',
+  components: { TransactionTable },
+  data() {
+    return {
+      currentPage: 0,
+      transactionPerPage: 20,
+      ascendentOrder: undefined,
+      selectedBank: undefined,
+      selectedAccount: undefined,
+    }
+  },
+  head() {
+    return { title: 'Transactions' }
+  },
 }
 </script>
